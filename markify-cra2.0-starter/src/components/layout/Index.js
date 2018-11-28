b4213cb1e4226fb40031df9358721c4486e17f91
Mark Soriano <merksmanship@gmail.com>
2018-11-27 22:49:40 -0800

    setup components

---
+++ markify-cra2.0-starter/src/components/layout/Index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import Class from '../folder/Component';
+
+// React.Fragment - fake dom div
+
+const Index = () => {
+	return (
+		<React.Fragment>
+
+		</React.Fragment>
+	);
+};
+
+export default Index;
